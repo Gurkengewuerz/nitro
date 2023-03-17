23c7eb0a4a226995a8e081e4eec2d9a3fdadae5d
Gurkengewuerz <niklas@schuetrumpf.net>
2023-03-17 22:22:58 +0100

    fix: close room engine on closed socket

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -76,9 +76,7 @@ export const App: FC<{}> = props => {
         setMessage("Connection Error");
         return;
       case NitroCommunicationDemoEvent.CONNECTION_CLOSED:
-        //if(GetNitroInstance().roomEngine) GetNitroInstance().roomEngine.dispose();
-        //setIsError(true);
-        setMessage("Connection Error");
+        if(GetNitroInstance().roomEngine) GetNitroInstance().roomEngine.dispose();
 
         HabboWebTools.send(-1, "client.init.handshake.fail");
 
