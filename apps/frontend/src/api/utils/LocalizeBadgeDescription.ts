1666e350a6cefbbbf35429d7bc4f710d87cead41
Gurkengewuerz <niklas@schuetrumpf.net>
2023-10-30 00:42:39 +0100

    fix: allow empty badge desc

--- apps/frontend/src/api/utils/LocalizeBadgeDescription.ts
+++ apps/frontend/src/api/utils/LocalizeBadgeDescription.ts
@@ -3,6 +3,8 @@ import {GetNitroInstance} from "..";
 export const LocalizeBadgeDescription = (key: string) => {
   let badgeDesc = GetNitroInstance().localization.getBadgeDesc(key);
 
+  if (badgeDesc === "") return badgeDesc;
+
   if (!badgeDesc || !badgeDesc.length) badgeDesc = `badge_desc_${key}`;
 
   return badgeDesc;
